7a0f5c17ec3ab94c86ffc4175168bb1151e31769
xixixida <1102120090@qq.com>
2017-07-21 09:29:39 +0800

    Add files via upload

---
+++ .autod.conf.js
@@ -0,0 +1,29 @@
+'ues strict';
+
+module.exports = {
+  write: true,
+  plugin: 'autod-egg',
+  prefix: '^',
+  devprefix: '^',
+  registry: 'https://r.cnpmjs.org',
+  exclude: [
+    'test/fixtures',
+    'examples',
+    "docs",
+  ],
+  devdep: [
+    'autod',
+    'autod-egg',
+    'eslint',
+    'eslint-config-egg',
+    'egg-bin',
+    'egg-plugin-puml',
+    'egg-view-nunjucks',
+  ],
+  keep: [
+  ],
+  semver: [
+    'koa-bodyparser@2',
+  ],
+  test: 'scripts',
+};
